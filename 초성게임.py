7bedc4e89d47375040b4c142859ecca2b1ba1281
sms211 <87003136+sms211@users.noreply.github.com>
2021-09-14 16:49:54 +0900

    20210914

---
+++ 초성게임.py
@@ -0,0 +1,85 @@
+print("stranger의 초성게임")
+while True :
+    startAnswer = input("게임을 시작하시겠습니까?")
+    if startAnswer == "예" :
+        print("[computer]:게임을 시작합니다")
+        break
+    elif startAnswer == "아니오" :
+        for x in range(10) :
+            print("[computer]:아디오스!")
+        exit()
+    else :
+        print("[computer]:오류")
+
+print("[computer]:첫번째 문제입니다")
+print("-----마인크래프트-----")
+print("ㅋㄹㅍ")
+
+while True :
+    answer1 = input()
+    if answer1 == "크리퍼" :
+        print("정답!")
+        break
+    elif answer1 == "힌트" :
+        print("Boom!")
+    else :
+        print("[computer]:오답입니다.모르겠다면 힌트라고 쳐 보세요")
+
+print("[computer]:두번째 문제 문제입니다")
+print("-----마인크래프트 노래(한국어 번역제목)-----")
+print("ㅇㅇ ㅅㅈㄱ ㄱㅇ")
+
+while True :
+    answer2 = input()
+    if answer2 == "용의 심장과 같이" :
+        print("정답")
+        break
+    elif answer2 == "힌트" :
+        print("dragonhearted(가수 tryhardninja,제작자 captainsparkles)")
+    else :
+          print("오답입니다.모르겠으면 힌트라고 쳐 보세요")
+          
+print("[computer]:세번째 문제 문제입니다")
+print("-----블로거-----")
+print("ㅂㄹㅇㅋ")
+
+while True :
+    answer2 = input()
+    if answer2 == "블루위키" : 
+        print("정답")
+        break
+    elif answer2 == "힌트" :
+        print("마인크래프트 괴담, 유튜버")
+    else :
+          print("오답입니다.모르겠으면 힌트라고 쳐 보세요")
+          
+print("[computer]:네번째 문제 문제입니다")
+print("-----게임회사-----")
+print("ㅁㅈ ㅅㅌㄷㅇ")
+
+while True :
+    answer2 = input()
+    if answer2 == "모장 스튜디오" : 
+        print("정답")
+        break
+    elif answer2 == "힌트" :
+        print("마르쿠스 페르손")
+    else :
+          print("오답입니다.모르겠으면 힌트라고 쳐 보세요")
+          
+print("[computer]:다섯번째 문제 문제입니다")
+print("-----프로그래밍 언어-----")
+print("ㅎㅇㅍ ㅌㅅㅌ ㅁㅋㅇ ㅇㅇ")
+
+while True :
+    answer2 = input()
+    if answer2 == "하이퍼 텍스트 마크업 언어" : 
+        print("정답")
+        break
+    elif answer2 == "힌트" :
+        print("HTML")
+    else :
+          print("오답입니다.모르겠으면 힌트라고 쳐 보세요")
+print("수고하셨습니다")
+          
+
